328b75f8ddeaf0cd733cd9b069f80d01a2368176
Guy Even <guyeven@Guys-MacBook-Pro.local>
2018-07-11 23:21:23 +0300

    adding templates to the project

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect('mongodb://localhost:27127');
+mongoose.connect('mongodb://localhost:27017', {useNewParser: true});
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/game', (req, res) => {
+  res.render('game', {game: res.query});
 });
 
 app.listen(3000, () => {
