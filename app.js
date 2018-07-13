728765908afa5bebbc831760889fa989836332b4
Guy Even <guyeven@Guys-MacBook-Pro.local>
2018-07-13 23:48:16 +0300

    Adding stylesheet.css and dotenv

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(`${__dirname}/public`));
 
-mongoose.connect('mongodb://localhost:27017', {useNewParser: true});
+mongoose.connect('mongodb://localhost:27017');
 
 app.get('/', (req, res) => {
   res.render('index');
