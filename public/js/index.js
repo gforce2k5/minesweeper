0f9bf5954573e77798fb9e5118ad7f42d36b06c3
Guy Even <guyeven@Guys-MacBook-Pro.local>
2018-07-25 00:06:36 +0300

    add difficulties to size selection

---
+++ public/js/index.js
@@ -0,0 +1,22 @@
+const difficulty = document.querySelector('#difficulty');
+
+difficulty.addEventListener('change', () => {
+  console.log(difficulty.value);
+  switch (difficulty.value) {
+    case 'easy':
+      setFields(9, 9, 10);
+      break;
+    case 'medium':
+      setFields(16, 16, 40);
+      break;
+    case 'hard':
+      setFields(31, 16, 99);
+      break;
+  }
+});
+
+setFields = (width, height, mines) => {
+  document.getElementById('width').value = `${width}`;
+  document.getElementById('height').value = `${height}`;
+  document.getElementById('mines').value = `${mines}`;
+};
