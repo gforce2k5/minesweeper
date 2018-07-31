5062ac8a0b3ae6b32b835876d2a376d56cc312e8
Guy Even <gforce2k5@gmail.com>
2018-07-31 21:31:40 +0300

    update game model

--- models/game.js
+++ models/game.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
   start: {type: Date, default: Date.now},
-  difficulty: Number,
+  difficulty: String,
 });
 
 module.exports = mongoose.model('Game', gameSchema);
